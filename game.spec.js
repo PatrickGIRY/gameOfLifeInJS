65af1c3ecff72f262ddfcba9ce4645546fcdcec3
Patrick GIRY <patrickgiry@gmail.com>
2018-10-03 17:57:16 +0200

    le code commis en séance du 03-10-2018

--- game.spec.js
+++ game.spec.js
@@ -1,9 +1,18 @@
 
-const nexState = grid => {
+const nextState = grid => {
 
+  if (compare(grid[0], [1, 0, 0]) &&
+      compare(grid[1], [1, 0, 0]) &&
+      compare(grid[2], [1, 0, 0])) {
+    return [
+        grid[0]
+      , [1,1,0]
+      , [1,0,0]
+      ];
+  }
   if (compare(grid[0], [1, 1, 1])) {
     return [
-        [1,1,1]
+        grid[0]
       , [0,1,0]
       , [0,0,0]
       ];
@@ -12,7 +21,7 @@ const nexState = grid => {
     return [
         [0,0,0]
       , [0,1,0]
-      , [1,1,1]
+      , grid[2]
       ];
   }
   return [
@@ -30,6 +39,26 @@ const compare = (a,b) => {
   }
   return true;
 }
+
+const countNeighborsCellsAlive = (grid, x, y) => {
+  let count = 0;
+  const mx = x + 2;
+  const my = y + 2;
+  for(let r = y - 1; r < my; r++) {
+    for(let c = x - 1; c < mx; c++) {
+      if (r >= 0) {
+        let row = grid[r];
+        if (r >= 0 && c >= 0 && row && row[c] == 1) {
+          if (c != x || r != y) {
+            count++;
+          }
+        }
+      }
+    }
+  }
+  return count;
+};
+
 describe ('Game of life', () => {
   it ('a cell died when it\'s alone', () => {
     let grid = [
@@ -38,21 +67,21 @@ describe ('Game of life', () => {
     , [0,0,0]
     ];
 
-    expect(nexState(grid)).toEqual([
+    expect(nextState(grid)).toEqual([
       [0,0,0]
     , [0,0,0]
     , [0,0,0]
     ]);
   });
 
-  it ('a cell is borne when cell has three neighbors', () => {
+  it ('a cell is born when cell has three neighbors alive', () => {
     let grid = [
       [1,1,1]
     , [0,0,0]
     , [0,0,0]
     ];
 
-    expect(nexState(grid)).toEqual([
+    expect(nextState(grid)).toEqual([
       [1,1,1]
     , [0,1,0]
     , [0,0,0]
@@ -64,10 +93,22 @@ describe ('Game of life', () => {
     , [1,1,1]
     ];
 
-    expect(nexState(grid)).toEqual([
+    expect(nextState(grid)).toEqual([
       [0,0,0]
     , [0,1,0]
     , [1,1,1]
+    ]);
+
+    grid = [
+      [1,0,0]
+    , [1,0,0]
+    , [1,0,0]
+    ];
+
+    expect(nextState(grid)).toEqual([
+      [1,0,0]
+    , [1,1,0]
+    , [1,0,0]
     ]);
   });
 });
@@ -79,5 +120,69 @@ describe ('compare array', () => {
   });
   it ('array are different', () => {
     expect(compare([1], [2])).toEqual(false);
+  });
+});
+
+describe('count neighbors cells alive', () => {
+  it('no neighbors cells alive', () => {
+   let grid = [
+      [0,0,0]
+    , [0,1,0]
+    , [0,0,0]
+    ];
+  expect(countNeighborsCellsAlive(grid, 1, 1)).toEqual(0);
+  });
+  it('one neighbors cells alive', () => {
+   let grid = [
+      [1,0,0]
+    , [0,1,0]
+    , [0,0,0]
+    ];
+  expect(countNeighborsCellsAlive(grid, 1, 1)).toEqual(1);
+   grid = [
+      [0,1,0]
+    , [0,1,0]
+    , [0,0,0]
+    ];
+  expect(countNeighborsCellsAlive(grid, 1, 1)).toEqual(1);
+   grid = [
+      [0,0,0]
+    , [1,1,0]
+    , [0,0,0]
+    ];
+  expect(countNeighborsCellsAlive(grid, 1, 1)).toEqual(1);
+   grid = [
+      [0,0,0,0]
+    , [1,0,1,0]
+    , [0,0,1,0]
+    , [0,0,0,0]
+    ];
+  expect(countNeighborsCellsAlive(grid, 2, 2)).toEqual(1);
+  grid = [
+     [0,0,0]
+   , [1,0,0]
+   , [0,0,0]
+   ];
+ expect(countNeighborsCellsAlive(grid, 0, 0)).toEqual(1);
+  grid = [
+     [0,0,0]
+   , [0,0,0]
+   , [0,1,0]
+   ];
+ expect(countNeighborsCellsAlive(grid, 2, 2)).toEqual(1);
+ grid = [
+    [0,0,0,0]
+  , [1,0,1,0]
+  , [0,0,1,0]
+  , [0,0,0,0]
+  ];
+expect(countNeighborsCellsAlive(grid, 3, 2)).toEqual(2);
+ grid = [
+    [0,0,0,0]
+  , [1,0,0,0]
+  , [0,0,1,1]
+  , [0,0,0,0]
+  ];
+expect(countNeighborsCellsAlive(grid, 2, 3)).toEqual(2);
   });
 });
